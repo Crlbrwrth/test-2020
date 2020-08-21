1025c1bc115938024057af5d41e06d058c8c4184
Crlbrwrth <carl.beerwerth@gmail.com>
2020-08-21 19:05:23 +0200

    make final changes

--- backend/lib/routes/ping.js
+++ backend/lib/routes/ping.js
@@ -1,6 +1,6 @@
 const handler = (request, h) => {
   console.log('Received ping request from ip address %s', request.info.remoteAddress);
-  return h.response({name: 'Peter', age: '22'}).code(200);
+  return h.response({senf: 3}).code(200);
 };
 
 module.exports = {
